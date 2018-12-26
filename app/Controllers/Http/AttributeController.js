b40e2499583a9103c3145a3112591a34ea281289
Maxim Rudenko <maxim.s.rudenko@gmail.com>
2018-12-26 13:52:06 +0200

    Attribute controller

---
+++ app/Controllers/Http/AttributeController.js
@@ -0,0 +1,29 @@
+const attributes = [{ phones: ['display', 'camera'] }, { TV: ['display', 'smart TV'] }, { 'washing machine': [] }];
+
+class AttributeController {
+  async show({ params }) {
+    return attributes[params.id];
+  }
+
+  async showAll() {
+    return attributes;
+  }
+
+  async add({ request }) {
+    console.log(attributes);
+    attributes.push(request.all());
+    return attributes;
+  }
+
+  async update({ params, request }) {
+    return attributes;
+  }
+
+  async delete({ params }) {
+    console.log(params);
+    attributes.splice(params.id, 1);
+    return attributes;
+  }
+}
+
+module.exports = AttributeController;
